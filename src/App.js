18869dc39b05a1fc6d973a7835dfb9de50159b68
Mayble Jun <mayblejul@gmail.com>
2023-02-28 20:11:56 +0400

    Add modal window and animation

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import MyInput from "./components/UI/input/MyInput";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 function App() {
 
   const [posts, setPosts] = useState([
-      {id:1, title: 'Mayble1', body: 'Description1'},
-      {id:1, title: 'Mayble2', body: 'Description2'},
-      {id:3, title: 'Mayble3', body: 'Description3'},
+      {id:1, title: 'Mayble Pines', body: 'Description1'},
+      {id:2, title: 'Dipper Pines', body: 'Description2'},
+      {id:3, title: 'Jesús Ramírez', body: 'Description3'},
   ])
 
     const [filter, setFilter] = useState({sort:'', query:''})
+    const [modal, setModal] = useState(false);
 
 
     const sortedPosts = useMemo(()=>{
@@ -29,7 +32,8 @@ function App() {
     },[filter.query,sortedPosts])
 
     const createPost = (newPost)=>{
-      setPosts([...posts,newPost])
+        setPosts([...posts,newPost])
+        setModal(false)
     }
 
     const removePost = (post) =>{
@@ -38,7 +42,12 @@ function App() {
 
     return (
     <div className="App">
-        <PostForm create={createPost} />
+        <MyButton style={{marginTop: 30}} onClick={()=> setModal(true)}>
+            Create user
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost} />
+        </MyModal>
         <hr style={{margin: '15px 0'}}/>
         <PostFilter
             filter={filter}
