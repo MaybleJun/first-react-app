1734b7979d8a61a86ad66e3f3c4372c65a6395d9
Mayble Jun <mayblejul@gmail.com>
2023-02-28 14:34:44 +0400

    Create app; add components: PostForm, PostItem, Postlist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useRef, useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 function App() {
-  return (
+
+  const [posts, setPosts] = useState([
+      {id:1, title: 'Mayble', body: 'Description'},
+      {id:1, title: 'Mayble2', body: 'Description'},
+      {id:3, title: 'Mayble3', body: 'Description'},
+  ])
+
+    const createPost = (newPost)=>{
+      setPosts([...posts,newPost])
+    }
+
+    const removePost = (post) =>{
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <PostForm create={createPost} />
+        <PostList remove={removePost} posts={posts} title="Персонажи Гравити Фолз"/>
+
     </div>
   );
 }
